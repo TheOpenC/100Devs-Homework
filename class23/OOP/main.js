45d1a73cce4722d70c38de1f4834bc923733da69
Drew Dudak <ddudak@gmail.com>
2024-11-13 16:20:34 -0500

    working on OOP homeowkr

--- class23/OOP/main.js
+++ class23/OOP/main.js
@@ -1,1 +1,12 @@
-console.log("hello worldddddd")
+
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw")
+    } 
+} 
+
+
+const another = new Circle(10);
+
