186a1883cc57d0c0d8afdbf9013f20b0444d5ba3
Drew Dudak <ddudak@gmail.com>
2024-11-14 12:24:50 -0500

    Homework for class 23 - Mosh's

--- class23/OOP/main.js
+++ class23/OOP/main.js
@@ -1,12 +1,34 @@
+function Stopwatch(){
+    let startTime, endTime, running, duration = 0;
+    
+    this.start = function () {
+        if (running)
+            throw new Error('Stopwatch has already started.');
+        running = true;
+        startTime = new Date();
+    };
 
+    this.stop = function () {
+        if (!running)
+            throw new Error('Stopwatch is not started.');
 
-function Circle(radius){
-    this.radius = radius;
-    this.draw = function() {
-        console.log("draw")
-    } 
-} 
+        running = false;
 
+        endTime = new Date();
 
-const another = new Circle(10);
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
 
+    this.reset = function () {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+
+    };
+
+    Object.defineProperty(this, 'duration', {
+
+    })
+}
