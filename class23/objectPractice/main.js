34fe8b1023b0c1542e0e4b10c69d15846600174f
Drew Dudak <ddudak@gmail.com>
2024-11-18 15:57:17 -0500

    Homework from Bro Code

---
+++ class23/objectPractice/main.js
@@ -0,0 +1,7 @@
+//object = A collection of related properties and/or methods
+    // can represent real world objects (people, products, places)
+    // object = {key: value, 
+    //        function()}
+    
+
+    
