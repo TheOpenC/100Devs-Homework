1c51897fe68128386de3513f3f59d5c1767d97f9
Drew Dudak <ddudak@gmail.com>
2024-12-03 16:11:14 -0500

    added 100devsCalc to ignore list

--- class26-APIs/class26-materials/objects-shots/js/main.js
+++ class26-APIs/class26-materials/objects-shots/js/main.js
@@ -2,10 +2,12 @@
 document.querySelector("button").addEventListener('click', getDrink)
 
 function getDrink(){
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
-    .then(res => res.json()) 
+    let drink = document.querySelector('input').value
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse json
     .then(data => {
-        console.log(data.drinks)
+        console.log(data.drinks[0])
         document.querySelector('h2').innerText = data.drinks[0].strDrink
         document.querySelector('img').src = data.drinks[0].strDrinkThumb
         document.querySelector('h3').innerText = data.drinks[0].strInstructions 
