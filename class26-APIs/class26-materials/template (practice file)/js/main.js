6c88fde8416c0e1d36b3bc91b28ae74e215659c7
Drew Dudak <ddudak@gmail.com>
2024-12-03 16:13:21 -0500

    removed 100Devs Calculator project as it was causing errors.

--- class26-APIs/class26-materials/template (practice file)/js/main.js
+++ class26-APIs/class26-materials/template (practice file)/js/main.js
@@ -1,16 +1,1 @@
-class MakeCar{
-  constructor(make, model, color, doors){
-    this.make = make
-    this.model = model
-    this.color = color
-    this.doors = doors
-  }
-  honk(){
-    alert('Beep Beep')
-  }
-  lock(){
-    alert(`Locked ${this.doors} doors!`)
-  }
-}
-
-let hondaCivic = new MakeCar('Honda', 'Civic', 'Silver', 4)
+// 
