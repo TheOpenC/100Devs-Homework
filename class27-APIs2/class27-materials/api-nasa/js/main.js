db5e8ac93bd0116cf99af23328dedca5071c82e0
Drew Dudak <ddudak@gmail.com>
2024-12-08 18:46:15 -0500

    nasa test

--- class27-APIs2/class27-materials/api-nasa/js/main.js
+++ class27-APIs2/class27-materials/api-nasa/js/main.js
@@ -3,12 +3,20 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=qe6RnddBSvUmBCgjcKtOibU1gOgNnnAdG2ulxzfr'
+  const url = `https://api.nasa.gov/planetary/apod?api_key=qe6RnddBSvUmBCgjcKtOibU1gOgNnnAdG2ulxzfr&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if (data.media_type === 'image'){
+          document.querySelector('img').src = data.hdurl
+
+        }else if(data.media_type === 'video' ){
+          document.querySelector('iframe').src = data.url
+        }
+        
+        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
