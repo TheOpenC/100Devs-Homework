5aa386c7cb25a1579038676179d877b09bec4a91
Drew Dudak <ddudak@gmail.com>
2024-12-10 21:29:43 -0500

    Card-game war

--- class27-APIs2/class27-materials/card-game/js/main.js
+++ class27-APIs2/class27-materials/card-game/js/main.js
@@ -1,22 +1,48 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+let deckId = ''
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://api.nasa.gov/planetary/apod?api_key=qe6RnddBSvUmBCgjcKtOibU1gOgNnnAdG2ulxzfr&date=${choice}`
+// this is how we get a deck
+fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+  .then(res => res.json())
+  .then(data => {
+    //get and store a deck id
+    console.log(data)
+    deckId = data.deck_id
+
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  });
+
+
+
+
+
+// click the button, draw two cards
+document.querySelector('button').addEventListener('click', drawTwo)
+
+
+function drawTwo(){
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if (data.media_type === 'image'){
-          document.querySelector('img').src = data.hdurl
+        // place the two drawn cards into the DOM
+        document.querySelector('#player1').src = data.cards[0].image
+        document.querySelector('#player2').src = data.cards[1].image
 
-        }else if(data.media_type === 'video' ){
-          document.querySelector('iframe').src = data.url
+        let play1Val = data.cards[0].value
+        let player2Val = data.cards[1].value
+        if(play1Val < player2Val){
+          document.querySelector('h3').innerText = 'Player 1 Wins'
+        }else if(player1Val < player2Val){
+           document.querySelector('h3').innerText = 'Player 2 Wins'
+        }else{
+          document.querySelector('h3').innerText = 'Time for War!'
         }
-        
-        document.querySelector('h3').innerText = data.explanation
+
       })
       .catch(err => {
           console.log(`error ${err}`)
