03854b13c82ce8c8ec9b95579817f48a3d001339
Drew Dudak <ddudak@gmail.com>
2025-02-05 20:51:40 -0500

    class 27 card game api

--- class27-APIs2/class27-materials/card-game/js/main.js
+++ class27-APIs2/class27-materials/card-game/js/main.js
@@ -6,7 +6,6 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
   .then(res => res.json())
   .then(data => {
     //get and store a deck id
-    console.log(data)
     deckId = data.deck_id
 
   })
@@ -14,44 +13,56 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     console.log(`error ${err}`)
   });
 
+  // on the click of the button, draw cards 
+ document.querySelector('button').addEventListener('click', drawTwo)
 
+// // get a new deck
+// document.querySelector('newDeckButton').addEventListener('click', newDeck())
 
+// function newDeck(){
+//   deckId = ""
+// }
 
-
-// click the button, draw two cards
-document.querySelector('button').addEventListener('click', drawTwo)
-
-
-function drawTwo(){
+ function drawTwo(){
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
-
+  console.log(deckId)
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        // place the two drawn cards into the DOM
-        document.querySelector('#player1').src = data.cards[0].image
-        document.querySelector('#player2').src = data.cards[1].image
+  .then(res => res.json())
+  .then(data => {
+    //get and store a deck id
+    console.log(data)
+    
 
-        let play1Val = convertToNum(data.cards[0].value)
-        let player2Val = convertToNum(data.cards[1].value)
-        
-        if(play1Val > player2Val){
-          document.querySelector('h3').innerText = 'Player 1 Wins'
-        }else if(player1Val < player2Val){
-           document.querySelector('h3').innerText = 'Player 2 Wins'
-        }else{
-          document.querySelector('h3').innerText = 'Time for War!'
-        }
+    document.querySelector('#player1').src = data.cards[0].image
+    document.querySelector('#player2').src = data.cards[1].image
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    let player1Card = `${data.cards[0].value} of ${data.cards[0].suit}`
+    let player1Val = convertToNum(data.cards[0].value)
+    let player2Card = `${data.cards[1].value} of ${data.cards[1].suit}`
+    let player2Val = convertToNum(data.cards[1].value)
+    if (player1Val > player2Val){
+      document.querySelector('h3').innerText = 'Player 1 Wins'
+      console.log(`Player 1 Wins`)
+    } else if(player1Val < player2Val){
+      document.querySelector('h3').innerText = 'Player 2 Wins'
+      console.log(`Player 2 Wins`)
+    } else{
+      document.querySelector('h3').innerText = 'WAR'
+      console.log(`WAR`)
+    }
+
+    console.log(`\nPlayer1: ${player1Card} \nPlayer2: ${player2Card}`)
+    console.log(``)
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  });
+
 }
 
 function convertToNum(val){
-  if(val === 'ACE'){
+  
+  if (val === 'ACE'){
     return 14
   }else if(val === 'KING'){
     return 13
@@ -59,7 +70,53 @@ function convertToNum(val){
     return 12
   }else if(val === 'JACK'){
     return 11
-  }else{
-    return Number(val)
+  } else {
+    return +val
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
