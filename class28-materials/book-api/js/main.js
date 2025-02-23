9cfec575d86f6a036adff799caf73e96ac1b7aa8
Drew Dudak <ddudak@gmail.com>
2025-02-22 19:36:42 -0500

    class 28 hw

--- class28-materials/book-api/js/main.js
+++ class28-materials/book-api/js/main.js
@@ -1,6 +1,8 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
-
+const li = document.createElement('li');
+li.textContent = localStorage.getItem('book')
+document.querySelector('ul').appendChild(li)
 
 function getFetch(){
   const choice = document.querySelector('input').value
@@ -11,11 +13,13 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        //put title into local storage 
+
         data.docs.forEach(book => {
+          localStorage.setItem('book', book.title)
           const li = document.createElement('li');
-          li.textContent = book.title
+          li.textContent = localStorage.getItem('book') + ' ; ' + data.title
           document.querySelector('ul').appendChild(li)
-          
         });
       })
       .catch(err => {
