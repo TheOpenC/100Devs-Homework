97069a9f1130a8269ea35462bc65391cf98538e6
Drew Dudak <62808741+TheOpenC@users.noreply.github.com>
2025-03-14 21:22:28 -0400

    homework class 30

--- class30 - OOP/class30-materials/objects-espresso-machine/js/main.js
+++ class30 - OOP/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class Espresso{
+    constructor(color, make, model, price) {
+        this.color = color 
+        this.make = make
+        this.model = model
+        this.price = price
+    } 
+    turnOn(){
+        console.log(`Good day, I am now on!`)
+    }
+    steam(){
+        console.log(`ITSSS SUMMER SALE 59 %%%%%%`)
+    }
+    brew(){
+        console.log(`Good Stuff coming your way.`)
+    }
+}
+
+let gaggia = new Espresso(`red`, `Gaggia`, `Classic Pro`, 400)
