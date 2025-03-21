a8895c6bea1e2285d3f6197412e96d4b802d209a
Drew Dudak <ddudak@gmail.com>
2025-03-21 15:39:19 -0400

    class 31 hw

--- class30 - OOP/class30-materials/objects-espresso-machine/js/main.js
+++ class30 - OOP/class30-materials/objects-espresso-machine/js/main.js
@@ -1,4 +1,5 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+<<<<<<< HEAD
 class Espresso{
     constructor(color, make, model, price) {
         this.color = color 
@@ -18,3 +19,25 @@ class Espresso{
 }
 
 let gaggia = new Espresso(`red`, `Gaggia`, `Classic Pro`, 400)
+=======
+
+class Espresso {
+ constructor(make, brand, material, power) {
+    this.make = make
+    this.brand = brand
+    this.material = material
+    this.power = power
+ } 
+    boil () {
+        `you are now boiling water with ${power}`
+    }
+    turnOn (){
+        `you turned on your beautiful ${brand} ${make}.`
+    }
+    froth (){
+        `time to make nice creamy milk for you coffee.`
+    }
+}
+
+let laMarzacco = new Espresso('v1', 'La Marzacco', 'stainless', 'gas')
+>>>>>>> 97bd185 (class 31 hw)
