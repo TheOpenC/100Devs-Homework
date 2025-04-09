54cb780f29e6a509c82d3304c57aa9d8977bdb31
Drew Dudak <ddudak@gmail.com>
2025-04-09 13:47:56 -0400

    class 31 homework

--- class31 - Inheritance and Polymorphism/class31-materials/complete/js/main.js
+++ class31 - Inheritance and Polymorphism/class31-materials/complete/js/main.js
@@ -36,12 +36,94 @@ class Cat extends Animal{
     }    
 }
 
+class Bird extends Animal {
+    constructor(name, breed) {
+        super(name)
+        this._breed = breed
+    }
+    get breed(){
+        return this._breed
+    }
+    speak(){
+        super.speak()
+        console.log(`${this.name} squawks`)
+    }
+}
+
 let simba = new Dog('Simba','Shepard')
 let machi = new Dog('The Machine','Pitbull')
 let salem = new Cat('Salem', 'American Shorthair')
+let chicky = new Bird('Chicky', 'Robin')
 
-let farm = [simba,machi,salem]
+let farm = [simba,machi,salem, chicky]
 
 for( a of farm ){
     a.speak()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
