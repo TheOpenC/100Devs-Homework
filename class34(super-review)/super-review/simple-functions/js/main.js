7362c7e89c71db8f6d1624e38172a8fdeaa20cbd
Drew Dudak <ddudak@gmail.com>
2025-09-21 22:07:11 -0400

    super review homework

--- class34(super-review)/super-review/simple-functions/js/main.js
+++ class34(super-review)/super-review/simple-functions/js/main.js
@@ -28,7 +28,7 @@ function takesThreeNums(n1, n2, n3){
 console.log(takesThreeNums(10,30, 7))
 
 //---Hard
-//create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+//create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number r
 function takesFourNums(n1, n2, n3, n4){
     let total = n1 * n2;
     if (total > 100){
