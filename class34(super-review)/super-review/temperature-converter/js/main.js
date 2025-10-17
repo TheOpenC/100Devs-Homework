04ffb34dd91f270e2da5f4280978dcd34b0ae9d2
Drew Dudak <ddudak@gmail.com>
2025-10-17 16:04:40 -0400

    super review hw

--- class34(super-review)/super-review/temperature-converter/js/main.js
+++ class34(super-review)/super-review/temperature-converter/js/main.js
@@ -11,3 +11,18 @@ function cToF (c){
 }
 
 cToF(c);
+
+
+document.querySelector('h1').addEventListener('click', cToF)
+
+function cToF(){
+    // need the value in c
+    let temp = Number(document.querySelector('input').value)
+
+    // convert c to f
+    temp = temp * 1.8 + 32
+
+    // show the value
+
+    document.querySelector('h2').innerText = temp
+}
