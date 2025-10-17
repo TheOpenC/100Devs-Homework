68647c1abe0b2a5cebea62fedf1d57f92f606dba
Drew Dudak <ddudak@gmail.com>
2025-10-17 15:55:45 -0400

    super review homework

--- class34(super-review)/super-review/temperature-converter/js/main.js
+++ class34(super-review)/super-review/temperature-converter/js/main.js
@@ -1,5 +1,13 @@
 //Write your pseduo code first! 
 // c to f
-
+// (C x 9/5) + 32 = F
 // need the value in c
 
+
+let c = Number(prompt('Pick a temp in Celcius'))
+
+function cToF (c){
+    console.log(`${c} Celcius =`,(c * 9/5) + 32, `Fahrenheit.`)
+}
+
+cToF(c);
