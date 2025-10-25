b4a5b2a2abef3d8b19faf79363ba2b9cd7deea08
Drew Dudak <ddudak@gmail.com>
2025-10-25 19:37:25 -0400

    APIs super review

--- class34(super-review)/super-review/objects-dog-photos/js/main.js
+++ class34(super-review)/super-review/objects-dog-photos/js/main.js
@@ -1,1 +1,24 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data.message)
+        document.querySelector('img').src = data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+
+    fetch(url)
+        .then(res => res.json()) //parse response as Json
+        .then(data => {
+            console.log()
+        })
+
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+    
+    
--- class34(super-review)/super-review/objects-street-fighter/js/main.js
+++ class34(super-review)/super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,25 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+
+class StreetFighter {
+    constructor(name, style, superPower, energyLvl){
+        this.name = name
+        this.style = style
+        this.superPower = superPower
+        this.energyLvl = energyLvl
+        this.intro = function(){
+            console.log(`${this.name} fighting in the style of ${this.style} has entered the ring!`)
+        }
+        this.usesSuper = function(){
+            console.log(`${this.name} uses ${this.superPower}!`)
+        }
+        this.block = function(){
+            console.log(`${this.name} blocks the attack, they're leftt unhurt~!`)
+        }
+
+    }
+}
+
+let fooFoo = new StreetFighter('Foo Foo', 'Wildstyle', 'Bite', "high")
+
+let barka = new StreetFighter('Barka', "Kung Fu", 'Long Kick', "god-like")
