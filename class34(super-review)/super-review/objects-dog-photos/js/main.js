98c2e7c1b9c17ca01da6ffe0b3234a3db9800783
Drew Dudak <ddudak@gmail.com>
2025-10-27 20:12:18 -0400

    apis on super reviuew

--- class34(super-review)/super-review/objects-dog-photos/js/main.js
+++ class34(super-review)/super-review/objects-dog-photos/js/main.js
@@ -1,20 +1,22 @@
-//Get a dog photo from the dog.ceo api and place the photo in the DOM
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-        console.log(data.message)
-        document.querySelector('img').src = data.message
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
+// //Get a dog photo from the dog.ceo api and place the photo in the DOM
+// fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//         console.log(data.message)
+//         document.querySelector('img').src = data.message
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     })
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
     fetch(url)
         .then(res => res.json()) //parse response as Json
         .then(data => {
-            console.log()
+            console.log(data.drinks[0])
+            document.querySelector('img').src = data.drinks[0].strDrinkThumb
         })
 
         .catch(err => {
