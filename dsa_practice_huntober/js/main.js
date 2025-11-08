98525821ff2a323382560ccd13eaf4980bf7f854
Drew Dudak <ddudak@gmail.com>
2025-11-08 11:19:39 -0500

    working through Node.js Homework class

--- dsa_practice_huntober/js/main.js
+++ dsa_practice_huntober/js/main.js
@@ -4,3 +4,20 @@
 // Methods Questions
 // 
 //Brute Force
+
+// algorithms -- steps you take to solve a problem. 
+//  some steps are more efficiently than others. 
+// Brute force first > then refine for space and time. 
+// space - how much memory is being consumed. 
+// time - Big O notation. Operations per input. 
+//  how much do we have to do for each operation? that's time. 
+
+// thinking through appropriate algorithms is how we can efficiently solve problems
+
+// Companies care about space & time  == less $$ for efficient code. 
+
+// we care about rough estimates -- not precision 
+
+// Complexities:
+// O(1) example
+
